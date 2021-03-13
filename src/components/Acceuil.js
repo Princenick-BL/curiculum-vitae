1dea7112a6c3cb01fcf24ca5595ba0bce6cabbe2
Nick <princenickballo@gmail.com>
2021-03-13 22:02:24 +0100

    V2 Acceuil Ok

--- src/components/Acceuil.js
+++ src/components/Acceuil.js
@@ -4,30 +4,30 @@ import MaPhoto from '../Media/MaPhoto.jpeg'
 
 export default function Acceuil() {
     return (
-        <div className="Acceuil">
+        <div className="Acceuil center">
             <div className="UsrImgContainer center">
                 <img className="UsrImg" src={MaPhoto} alt="logo"></img>
             </div>
 
-            <div className="Info">BALLO Prince Nick</div>
-            <div className="ForTitre">20 ans</div>
+            <div className="Nom">BALLO Prince Nick</div>
+            <div className="Age">20 ans</div>
             
             <div className="UserInfo">
                 
-                <div className="Info UserName">
-                    <div className="Titre Email">Email</div>
+                <div className="Info Email">
+                    <div className="Titre ">Email</div>
                     <div className="ForTitre">princenickballo@gmail.com</div>
                 </div>
-                <div className="Info UserName">
-                    <div className="Titre LinkedIn">LinkedIn</div>
+                <div className="Info LinkedIn">
+                    <div className="Titre ">LinkedIn</div>
                     <div className="ForTitre"><a href="https://www.linkedin.com/in/prince-nick-ballo/">Redirection vers LinkedIn</a></div>
                 </div>
-                <div className="Info UserName">
-                    <div className="Titre Adress">Adresse</div>
+                <div className="Info Adress">
+                    <div className="Titre ">Adresse</div>
                     <div className="ForTitre">100 rue Diderot 62100 Calais</div>
                 </div>
-                <div className="Info UserName">
-                    <div className="Titre Tel">Tel</div>
+                <div className="Info Tel">
+                    <div className="Titre ">Tel</div>
                     <div className="ForTitre">07 54 50 96 49</div>
                 </div>
         
